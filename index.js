36fd19437d7bf26e8f29d1f1bbd710e6be56f930
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-05 13:32:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Sam", "Don", "Ken"];
+
+  function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+      console.log(`Thank you, ${names[i]},for the wonderful birthday gift!`);
+      debugger;
+    }
+  
+    return names;
+  }
+  
+  writeCards(gifts);
+
+let countdown = 10;
+while (countdown > 0) {
+  console.log(countdowm--);
+}
