515d690906349ea68d2e466f2104e8a95c711189
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-08 15:14:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"]
+
+//   function writeCards(names) {
+//     for (let i = 0; i < names.length; i++) {
+//       console.log(`Thank you, ${names[i]},for the wonderful birthday gift!`);
+//       debugger;
+//     }
+  
+//     return names;
+//   }
+  
+//   writeCards(names);
+
+let countdown = 10;
+while (countdown > -1) {
+  console.log(countdown--);
+}
+ 
 const names = ["Guadalupe", "Ollie", "Aki"]
 
-  function writeCards(names) {
-    for (let i = 0; i < names.length; i++) {
-      console.log(`Thank you, ${names[i]},for the wonderful birthday gift!`);
-      debugger;
+function writeCards(names) {
+    let i = 0;
+    while (i < names.length) {
+        console.log(`Thank you ${names[i]}, for the wonderful birthday gift!`);
+        i++;
+        debugger;
     }
-  
-    return names;
-  }
-  
-  writeCards(names);
+    return (names)
+}
+writeCards(names)
+
+for (let i = 0; i< 5; i++){
+  console.log(i)
+}
---
+++ test1.js
@@ -0,0 +1,21 @@
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+    let i = 0;
+    while (i < names.length) {
+        console.log(`Thank you ${names[i]}, for the wonderful birthday gift!`);
+        i++;
+        debugger;
+    }
+    return (names)
+}
+writeCards(names)
+
+for (let i = 0; i< 5; i++){
+    console.log(i)
+}
+
+
+
+
